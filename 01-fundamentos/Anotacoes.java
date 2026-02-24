cb1df990128aa59a40cfd56a01731898d7397790
Madu Silva Teixeira <m.eduardateixeirasilva@outlook.com>
2026-02-24 18:54:05 -0300

    anotações iniciais

---
+++ 01-fundamentos/Anotacoes.java
@@ -0,0 +1,9 @@
+// Priemeiras letras maisculas são usadas para nomear classes ou interfaces, como "Anotacoes". O nome do arquivo deve ser o mesmo da classe pública contida nele. PRIMEIRA LETRA MAIUSCULA É O PADRÃO PARA CLASSES E INTERFACES.
+
+// Se a primeira letra for minuscula, o Java entenderá que se trata de um método ou variável, e não de uma classe. Por exemplo, "anotacoes" seria interpretado como um método ou variável, e não como uma classe.
+
+// todas as letras minusculas são usadas para nomear métodos ou variáveis, como "main" ou "args". O nome do arquivo pode ser diferente do nome do método ou variável, mas é recomendado que seja o mesmo para facilitar a identificação.
+
+// todas as letras maisvulsculas são usadas para nomear constantes, como "PI" ou "TAXA_DE_JUROS". O nome do arquivo pode ser diferente do nome da constante, mas é recomendado que seja o mesmo para facilitar a identificação.
+
+
