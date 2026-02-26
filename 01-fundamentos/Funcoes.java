558915a72cceac6d4adf8ec3dfe274ea8d0fdc90
Madu Silva Teixeira <m.eduardateixeirasilva@outlook.com>
2026-02-26 19:43:56 -0300

    funções

---
+++ 01-fundamentos/Funcoes.java
@@ -0,0 +1,25 @@
+
+public class Funcoes {
+    public static void main(String[] args) {
+        cal(100, 10);
+    }
+
+        static void cal(int x, int y) {
+            System.out.println("Soma: " + (x + y));
+            System.out.println("Divisão: " + (x / y));
+            System.out.println("Multiplicação: " + (x * y));
+            System.out.println("Subtração: " + (x - y));
+        }
+    }
+
+
+// costumamos chamar funcoes através de verbos,função "sum" é um verbo que indica a ação de somar.
+
+// static void = declaração de função.
+
+//  O nome da função é "sum" e ela recebe dois parâmetros do tipo inteiro (int a, int b). Sempre os parâmetros são indicados dentro de parenteses, separados por vírgula e o tipo de dado é declarado antes do nome do parâmetro. (tipo nome, tipo nome)
+
+// {} para iniciar bloco de código da função.
+
+//  Dentro da função, o resultado da divisão de x e y é impresso no console usando System.out.println().
+
