1a397e71b89b38304385d12f14a826349e5c967a
Madu Silva Teixeira <m.eduardateixeirasilva@outlook.com>
2026-02-26 18:51:25 -0300

    variaveis e tipos de dados

---
+++ 01-fundamentos/VariaveisTipos.java
@@ -0,0 +1,26 @@
+public class VariaveisTipos {
+    public static void main(String[] args) {
+        var name = "Madu";
+        var age = 17;
+        var height = 1.55;
+        var student = true;
+
+        System.out.println("Nome: " + name);
+        System.out.println("Idade: " + age);
+        System.out.println("Altura: " + height);
+        System.out.println("Estudante: " + student);
+    }
+} 
+
+
+// Variaveis são espaços na memória do computador onde podemos armazenar dados temporariamente.
+
+// String = variável do tipo texto
+// Int = variável do tipo inteiro
+// Double = variável do tipo número decimal
+// Boolean = variável do tipo verdadeiro ou falso
+
+// Com java moderno não é necessário declarar o tipo da variável, o Java é capaz de inferir o tipo a partir do valor atribuído. Por exemplo, ao declarar "var name = "João";", o Java entende que "name" é uma variável do tipo String.
+
+// Preferencia a colocar nomes diretos as variáveis, como "name" para nome. E dar prefencia a escrever em ingles, para melhorar o alcance e profisionalismo do código.
+
