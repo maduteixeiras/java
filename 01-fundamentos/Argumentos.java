afc7d059203c22219b0920a03dbb36f5da167fc4
Madu Silva Teixeira <m.eduardateixeirasilva@outlook.com>
2026-02-26 23:25:03 -0300

    Argumentos e Compilando pelo Terminal com passagem de parâmetros

---
+++ 01-fundamentos/Argumentos.java
@@ -0,0 +1,17 @@
+public class Argumentos {
+    public static void main(String [] args) {
+        System.out.println("Name: " +args[0]);
+        System.out.println("Age: " + args[1]);
+        System.out.println("Heigth: " + args[2]);
+        System.out.println("Weight: " + args[3]);
+
+
+    } 
+}
+
+// Passando argumentos para o programa através do terminal: 
+// cd nome-da-pasta (Exemplo - cd 01-fundamentos)
+// dir (Verificar os arquivos da pasta)
+// javac nomedoarquivo.java (Compilar o código)
+// java Argumentos >Arg1 Arg2 ...< (Passando os argumentos para o programa, separados por espaço)
+
