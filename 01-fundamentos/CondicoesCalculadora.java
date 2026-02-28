80a997e4be84a689634a697cced330ab6badcd8c
Madu Silva Teixeira <m.eduardateixeirasilva@outlook.com>
2026-02-28 14:25:16 -0300

    fix: if, else, else if + calculate

--- 01-fundamentos/CondicoesCalculadora.java
+++ 01-fundamentos/CondicoesCalculadora.java
@@ -1,10 +1,12 @@
 public class CondicoesCalculadora {
     public static void main(String [] args) {
+        // envelopar: é o processo de pegar um valor primitivo e transformá-lo em um objeto. Acima, não poderiamos chamar o método sum diretamente com args[0] e args[1], pois eles são do tipo String. Então, precisamos convertê-los para int usando Integer.parseInt(), que é um processo de envelopamento.
         // Envelopando:
         int x = Integer.parseInt(args[1]);
         int y = Integer.parseInt(args[2]);
 
         // Condicionais if - else (se - senão)
+        // else depende da condição do if, else if depende da condição if ser false e da condição do else if ser true.
             if (args[0].equals("sum")) {
         sum(x, y);
         }   else if (args[0].equals("sub"))
@@ -22,4 +24,7 @@ public class CondicoesCalculadora {
     }
 }
 
-// envelopar: é o processo de pegar um valor primitivo e transformá-lo em um objeto. Acima, não poderiamos chamar o método sum diretamente com args[0] e args[1], pois eles são do tipo String. Então, precisamos convertê-los para int usando Integer.parseInt(), que é um processo de envelopamento.
+
+// OPERADORES LÓGICOS BOOLEANOS:
+// && - operador lógico AND (E)
+// || - operador lógico OR (OU)
